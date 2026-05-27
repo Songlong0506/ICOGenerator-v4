6aff5835f349191849464bc6a80d35fbc6f06d80
Songlong0506 <34188445+Songlong0506@users.noreply.github.com>
2026-05-27 23:32:06 +0700

    Create SrsDto.cs

---
+++ Services/Models/SrsDto.cs
@@ -0,0 +1,20 @@
+namespace ICOGenerator.Services.Models
+{
+    public class SrsDto
+    {
+        public string ProjectName { get; set; } = "";
+        public string Purpose { get; set; } = "";
+        public string Scope { get; set; } = "";
+        public string UserGroups { get; set; } = "";
+        public string Assumptions { get; set; } = "";
+        public string Constraints { get; set; } = "";
+        public string FunctionalRequirements { get; set; } = "";
+        public string NonFunctionalRequirements { get; set; } = "";
+        public string UiRequirements { get; set; } = "";
+        public string ApiRequirements { get; set; } = "";
+        public string DataRequirements { get; set; } = "";
+        public string DeploymentRequirements { get; set; } = "";
+        public string TestingRequirements { get; set; } = "";
+        public string OpenIssues { get; set; } = "";
+    }
+}
