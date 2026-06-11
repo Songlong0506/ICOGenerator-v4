ef9386c5098021e16840f5719d0baf1e788faa02
Songlong0506 <34188445+Songlong0506@users.noreply.github.com>
2026-06-12 00:12:17 +0700

    Create ChatWithBAUseCase.cs

---
+++ Application/Requirements/ChatWithBAUseCase.cs
@@ -0,0 +1,16 @@
+using ICOGenerator.Services.Requirements;
+
+namespace ICOGenerator.Application.Requirements;
+
+public class ChatWithBAUseCase
+{
+    private readonly BARequirementService _baRequirementService;
+
+    public ChatWithBAUseCase(BARequirementService baRequirementService)
+    {
+        _baRequirementService = baRequirementService;
+    }
+
+    public Task ExecuteAsync(Guid projectId, string message) =>
+        _baRequirementService.ChatAsync(projectId, message);
+}
